a3d653b3adf3e095fcfa54bb11f9f3d23d85a573
NeroGnu <xu.xian163@live.cn>
2015-07-01 17:56:18 +0800

    7-1

--- assign/src/org/buaa/xuxian/assign/Matrix.java
+++ assign/src/org/buaa/xuxian/assign/Matrix.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 public class Matrix {
 	int row;
 	int col;
+	Index max_index;
 	double data[][];
 	
 	public Matrix(int i, int j){
 		this.row = i;
 		this.col = j;
+		this.max_index = new Index(i, j);
 		this.data = new double[this.row][this.col];
 		for (int counter = 0; counter < this.row; counter++){
 			Arrays.fill(this.data[counter], 0);
@@ -19,19 +21,22 @@ public class Matrix {
 	public Matrix(int i, int j, double value){
 		this.row = i;
 		this.col = j;
+		this.max_index = new Index(i, j);
 		this.data = new double[this.row][this.col];
 		for (int counter = 0; counter < this.row; counter++){
 			Arrays.fill(this.data[counter], value);
 		}
 	}
 	
-	public void SetRow(int row, double value){
+	private void SetRow(int row, double value){
 		Arrays.fill(this.data[row], value);
 	}
 	
-	public void SetCol(int col, double value){
+	private void SetCol(int col, double value){
 		
 	}
+	
+	
 	
 	
 	
@@ -45,4 +50,31 @@ public class Matrix {
 		}
 	}
 
+}
+
+ class Index {
+	protected int row_index[];
+	protected double row_value[];
+	protected int col_index[];
+	protected double col_value[];
+	protected int i;
+	protected int j;
+	protected double value;
+	
+	protected Index(int m, int n){
+		
+		this.row_index = new int[m];
+		Arrays.fill(this.row_index, -1);
+		this.row_value = new double[m];
+		Arrays.fill(this.row_value, 0);
+		
+		this.col_index = new int[n];
+		Arrays.fill(this.col_index, -1);
+		this.col_value = new double[n];
+		Arrays.fill(this.col_value, 0);
+		
+		this.i = -1;
+		this.j = -1;
+		this.value = 0;
+	}
 }
